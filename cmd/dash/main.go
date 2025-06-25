ee566d5e2c91c03ad84d88ede65a6184acb17acf
Alex Suraci <suraci.alex@gmail.com>
2025-06-25 16:02:26 -0400

    use cobra + fang
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- cmd/dash/main.go
+++ cmd/dash/main.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 
 	"dagger.io/dagger"
+	"github.com/charmbracelet/fang"
 	"github.com/dagger/dagger/codegen/introspection"
+	"github.com/spf13/cobra"
 	"github.com/vito/dash/pkg/dash"
 )
 
-func main() {
-	var debug bool
-	flag.BoolVar(&debug, "debug", false, "enable debug logging")
-	flag.Parse()
+// Config holds the application configuration
+type Config struct {
+	Debug bool
+	File  string
+}
 
+func main() {
+	var cfg Config
+
+	// Create the root command
+	rootCmd := &cobra.Command{
+		Use:   "dash [flags] <file>",
+		Short: "Dash language interpreter",
+		Long: `Dash is a functional language for building Dagger pipelines.
+It provides type-safe, composable abstractions for container operations.`,
+		Example: `  # Run a Dash script
+  dash script.dash
+
+  # Run with debug logging enabled
+  dash --debug script.dash
+  dash -d script.dash`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cfg.File = args[0]
+			return run(cfg)
+		},
+	}
+
+	// Add flags
+	rootCmd.Flags().BoolVarP(&cfg.Debug, "debug", "d", false, "Enable debug logging")
+
+	// Use fang for styled execution with enhanced features
+	ctx := context.Background()
+	if err := fang.Execute(ctx, rootCmd,
+		fang.WithVersion("v0.1.0"),
+		fang.WithCommit("dev"),
+	); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(cfg Config) error {
 	// Set up slog with appropriate level
 	level := slog.LevelInfo
-	if debug {
+	if cfg.Debug {
 		level = slog.LevelDebug
 	}
-	
+
 	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: level,
 	})
@@ -31,28 +69,26 @@ func main() {
 
 	ctx := context.Background()
 
-	args := flag.Args()
-	if len(args) < 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [--debug] <dash-file>\n", os.Args[0])
-		os.Exit(1)
-	}
-
+	// Connect to Dagger
 	dag, err := dagger.Connect(ctx)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to connect to Dagger: %w", err)
 	}
 	defer dag.Close()
 
+	// Introspect the GraphQL schema
 	schema, err := Introspect(ctx, dag)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to introspect schema: %w", err)
 	}
 
-	if err := dash.CheckFile(schema, dag, args[0]); err != nil {
-		panic(err)
+	// Check and evaluate the Dash file
+	if err := dash.CheckFile(schema, dag, cfg.File); err != nil {
+		return fmt.Errorf("failed to evaluate Dash file: %w", err)
 	}
 
 	fmt.Println("ok!")
+	return nil
 }
 
 func Introspect(ctx context.Context, dag *dagger.Client) (*introspection.Schema, error) {
--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ toolchain go1.24.4
 
 require (
 	dagger.io/dagger v0.18.11
+	github.com/charmbracelet/fang v0.2.0
 	github.com/chewxy/hm v1.0.0
 	github.com/dagger/dagger v0.8.7
 	github.com/iancoleman/strcase v0.3.0
 	github.com/pkg/errors v0.9.1
-	github.com/smacker/go-tree-sitter v0.0.0-20230501083651-a7d92773b3aa
+	github.com/spf13/cobra v1.9.1
 )
 
 require (
@@ -19,13 +20,30 @@ require (
 	github.com/adrg/xdg v0.5.3 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.2 // indirect
+	github.com/charmbracelet/colorprofile v0.3.0 // indirect
+	github.com/charmbracelet/lipgloss/v2 v2.0.0-beta.1 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
+	github.com/charmbracelet/x/exp/charmtone v0.0.0-20250603201427-c31516f43444 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/mango v0.1.0 // indirect
+	github.com/muesli/mango-cobra v1.2.0 // indirect
+	github.com/muesli/mango-pflag v0.1.0 // indirect
+	github.com/muesli/roff v0.1.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.28 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/xtgo/set v1.0.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/otel v1.36.0 // indirect
