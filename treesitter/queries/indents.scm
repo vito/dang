752b4890e0d04a0d8d803463f29de8ad6674fb98
Alex Suraci <suraci.alex@gmail.com>
2025-07-04 21:39:35 -0400

    fix treesitter/zed indent rules
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- treesitter/queries/indents.scm
+++ treesitter/queries/indents.scm
@@ -1,20 +1,41 @@
+;; Indent on structural elements
 [
-  (list)
   (block)
+  (list)
+  (class)
+  (conditional)
+  (else_clause)
+  (lambda)
+  (match)
+  (object)
+  (paren_form)
+  (arg_values)
+  (arg_types)
+  (lambda_args)
+  (pattern_args)
+  (message_args)
 ] @indent
 
+;; Indent on opening brackets
 [
-  ")"
+  "{"
+  "["
+  "("
+] @indent
+
+;; Dedent on closing brackets
+[
   "}"
   "]"
-] @indent_end
+  ")"
+] @outdent
 
+;; Branch pairs for balanced indentation
 [ "(" ")" ] @branch
-
 [ "{" "}" ] @branch
-
 [ "[" "]" ] @branch
 
+;; Auto-indent for errors and comments
 [
   (ERROR)
   (comment_token)
--- zed-bind/languages/bind/indents.scm
+++ zed-bind/languages/bind/indents.scm
@@ -1,3 +1,21 @@
+;; Indent on structural elements
+[
+  (block)
+  (list)
+  (class)
+  (conditional)
+  (else_clause)
+  (lambda)
+  (match)
+  (object)
+  (paren_form)
+  (arg_values)
+  (arg_types)
+  (lambda_args)
+  (pattern_args)
+  (message_args)
+] @indent
+
 ;; Indent on opening brackets
 [
   "{"
