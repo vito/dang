d6c663e11c8b2fa3a57530a8fcf3cbb0a9c9b9fb
Alex Suraci <suraci.alex@gmail.com>
2025-10-16 18:15:06 -0700

    zed: non-broken indent rules
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- zed-dang/languages/dang/indents.scm
+++ zed-dang/languages/dang/indents.scm
@@ -1,31 +1,3 @@
-;; Indent on structural elements
-[
-  (block)
-  (list)
-  (class)
-  (conditional)
-  (else_clause)
-  (lambda)
-  (match)
-  (object)
-  (paren_form)
-  (arg_values)
-  (arg_types)
-  (lambda_args)
-  (pattern_args)
-  (message_args)
-] @indent
-
-;; Indent on opening brackets
-[
-  "{"
-  "["
-  "("
-] @indent
-
-;; Dedent on closing brackets
-[
-  "}"
-  "]"
-  ")"
-] @outdent
+(_ "{" "}" @end) @indent
+(_ "[" "]" @end) @indent
+(_ "(" ")" @end) @indent
