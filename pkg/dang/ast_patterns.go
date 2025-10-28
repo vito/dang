88b6840899f1a84bbf26592818645b4f5f139458
Alex Suraci <suraci.alex@gmail.com>
2025-10-28 13:16:59 -0400

    match: better error annotation
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- pkg/dang/ast_patterns.go
+++ pkg/dang/ast_patterns.go
@@ -11,7 +11,7 @@ import (
 type Match struct {
 	InferredTypeHolder
 	Expr  Node
-	Cases []MatchCase
+	Cases []*MatchCase
 	Loc   *SourceLocation
 }
 
@@ -36,41 +36,45 @@ func (m *Match) Body() hm.Expression { return m }
 func (m *Match) GetSourceLocation() *SourceLocation { return m.Loc }
 
 func (m *Match) Infer(ctx context.Context, env hm.Env, fresh hm.Fresher) (hm.Type, error) {
-	exprType, err := m.Expr.Infer(ctx, env, fresh)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(m.Cases) == 0 {
-		return nil, fmt.Errorf("Match.Infer: no match cases")
-	}
-
-	var resultType hm.Type
-	for i, case_ := range m.Cases {
-		caseEnv := env.Clone()
-
-		// TODO: Pattern matching type checking - for now just add pattern variables
-		if varPattern, ok := case_.Pattern.(VariablePattern); ok {
-			caseEnv.Add(varPattern.Name, hm.NewScheme(nil, exprType))
-		}
-
-		caseType, err := case_.Expr.Infer(ctx, caseEnv, fresh)
+	return WithInferErrorHandling(m, func() (hm.Type, error) {
+		exprType, err := m.Expr.Infer(ctx, env, fresh)
 		if err != nil {
 			return nil, err
 		}
 
-		if i == 0 {
-			resultType = caseType
-		} else {
-			subs, err := hm.Unify(resultType, caseType)
-			if err != nil {
-				return nil, fmt.Errorf("Match.Infer: case %d type mismatch: %s != %s", i, resultType, caseType)
-			}
-			resultType = resultType.Apply(subs).(hm.Type)
+		if len(m.Cases) == 0 {
+			return nil, fmt.Errorf("Match.Infer: no match cases")
 		}
-	}
 
-	return resultType, nil
+		var resultType hm.Type
+		for i, case_ := range m.Cases {
+			caseEnv := env.Clone()
+
+			// TODO: Pattern matching type checking - for now just add pattern variables
+			if varPattern, ok := case_.Pattern.(VariablePattern); ok {
+				caseEnv.Add(varPattern.Name, hm.NewScheme(nil, exprType))
+			}
+
+			caseType, err := WithInferErrorHandling(case_, func() (hm.Type, error) {
+				return case_.Expr.Infer(ctx, caseEnv, fresh)
+			})
+			if err != nil {
+				return nil, err
+			}
+
+			if i == 0 {
+				resultType = caseType
+			} else {
+				subs, err := hm.Unify(resultType, caseType)
+				if err != nil {
+					return nil, WrapInferError(fmt.Errorf("Match.Infer: case %d type mismatch: %s != %s", i, resultType, caseType), case_)
+				}
+				resultType = resultType.Apply(subs).(hm.Type)
+			}
+		}
+
+		return resultType, nil
+	})
 }
 
 func (m *Match) Walk(fn func(Node) bool) {
@@ -148,6 +152,13 @@ type MatchCase struct {
 	InferredTypeHolder
 	Pattern Pattern
 	Expr    Node
+	Loc     *SourceLocation
+}
+
+var _ SourceLocatable = (*MatchCase)(nil)
+
+func (c *MatchCase) GetSourceLocation() *SourceLocation {
+	return c.Loc
 }
 
 // WildcardPattern represents the wildcard pattern '_'
