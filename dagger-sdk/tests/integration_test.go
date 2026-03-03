8521a6abcd4ab8cf59aff4e1b4684a3facf5597d
Vasek - Tom C <tom@dagger.io>
2026-03-03 10:54:36 -0800

    chore: update the Dang SDK to use `github.com/dagger/otel-go`.
    With the recent issue on https://github.com/dagger/dagger/pull/11937, we moved
    dagger.io/dagger/telemetry to its own repo: github.com/dagger/otel-go.
    This PR updates the Dang SDK to use that new package so we can
    continue the bump on our repo.
    
    Signed-off-by: Vasek - Tom C <tom@dagger.io>

--- dagger-sdk/tests/integration_test.go
+++ dagger-sdk/tests/integration_test.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"testing"
 
-	"dagger.io/dagger/telemetry"
+	telemetry "github.com/dagger/otel-go"
 	"github.com/dagger/testctx"
 	"github.com/dagger/testctx/oteltest"
 	"github.com/stretchr/testify/require"
